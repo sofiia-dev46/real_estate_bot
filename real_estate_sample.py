1870feef5cee61afed45e16035f69471ccf4af66
sofiia.radul <sofiia.radul@osapiens.com>
2025-07-13 11:13:04 +0200

    TASK-2: added sample test

---
+++ real_estate_sample.py
@@ -0,0 +1,2 @@
+class RealEstateSample:
+    name: str
---
+++ test_real_estate_sample.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+from real_estate_sample import RealEstateSample
+
+
+class TestRealEstateSample(TestCase):
+    def test_real_estate_sample(self):
+        assert RealEstateSample()
